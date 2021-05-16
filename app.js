ea9c801272f482018723c37161e958c9fcd859f9
Karan Mahajan <codeofkaran@gmail.com>
2021-05-16 15:30:15 +0530

    image uploaded

--- app.js
+++ app.js
@@ -11,9 +11,8 @@ function getTranslationURL(inputText){
 function clickHandler(){
     var input = inputTxt.value;
     fetch(getTranslationURL(input))
-    .then(response => response.json())
+    .then(response => response.json()) // Here we get the response in object form so we will convert it into JSON and pass it to output
     .then(json => outputTxt.innerText = json.contents.translated)
 };
 
 button.addEventListener("click", clickHandler);
-
